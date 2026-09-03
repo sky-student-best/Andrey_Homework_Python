ed83fdd4a2635552fe21ea7f458be03a96046153
sky-student-best <andr-g09@yandex.ru>
2026-09-03 22:06:04 +0300

    feat: add lesson1 tasks

--- Lesson_01/lesson_1_task_3.py
+++ Lesson_01/lesson_1_task_3.py
@@ -1,5 +1,4 @@
 first_name = input("Введите ваше имя: ")
 last_name = input("Введите вашу фамилию: ")
 
-print(f"Меня зовут:, {last_name} {first_name}")
-
+print(f"Меня зовут: {last_name} {first_name}")
